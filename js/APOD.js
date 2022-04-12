261fd62337f74e674d748b72b7e7fa93a8f59f02
LorraneRochaS <lorraner.s@gmail.com>
2022-04-12 17:17:35 -0300

    Js -não está pronto, erro

--- js/APOD.js
+++ js/APOD.js
@@ -3,5 +3,18 @@ let url = "https://api.nasa.gov/planetary/apod?" ;
 let chave = "https://api.nasa.gov/planetary/apod";
 
 
+$("#pesquisar").click( function (event) {
+    event.preventDefault()
+});
 
+function resultado (result) {
+    $.ajax ({
+        url: `https://api.nasa.gov/planetary/apod? api_key=bcYLQye5tN4bG30NzAiP8Y0OWJSlgEaY1ndxDS3Q=${result}`,
+ sucess: function(resposta) {
+     $("#nome").text(result.date)
+     $("#imagem").attr("src", result.url)
+
+ }
+    })
+}
 

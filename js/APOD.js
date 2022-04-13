df64aebfea388b36ff610d825bc0aa9fd0d9d9f2
LorraneRochaS <lorraner.s@gmail.com>
2022-04-13 15:07:28 -0300

    Começando novamente a logica

--- js/APOD.js
+++ js/APOD.js
@@ -1,20 +1,8 @@
-let  data = $("#data").val();
-let url = "https://api.nasa.gov/planetary/apod?" ;
-let chave = "https://api.nasa.gov/planetary/apod";
+let data = $("#data").val();
+let url = "https://api.nasa.gov/planetary/apod?";
+let chave = "bcYLQye5tN4bG30NzAiP8Y0OWJSlgEaY1ndxDS3Q";
 
 
-$("#pesquisar").click( function (event) {
-    event.preventDefault()
-});
 
-function resultado (result) {
-    $.ajax ({
-        url: `https://api.nasa.gov/planetary/apod? api_key=bcYLQye5tN4bG30NzAiP8Y0OWJSlgEaY1ndxDS3Q=${result}`,
- sucess: function(resposta) {
-     $("#nome").text(result.date)
-     $("#imagem").attr("src", result.url)
 
- }
-    })
-}
 

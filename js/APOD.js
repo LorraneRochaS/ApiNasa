06b1fb1bb8185123398fd423d66421a96e903870
LorraneRochaS <lorraner.s@gmail.com>
2022-04-13 16:19:40 -0300

    Js Funcionol, ajustes no html, exclui button e <span>

--- js/APOD.js
+++ js/APOD.js
@@ -1,8 +1,16 @@
-let data = $("#data").val();
-let url = "https://api.nasa.gov/planetary/apod?";
-let chave = "bcYLQye5tN4bG30NzAiP8Y0OWJSlgEaY1ndxDS3Q";
+$("#submit").click(function (event) {
+    event.preventDefault()
+    const data = $("#data").val();
 
 
 
 
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?date=${data}&api_key=bcYLQye5tN4bG30NzAiP8Y0OWJSlgEaY1ndxDS3Q`,
+        success: function (result) {
+            $("#nome").text(result.title)
+            $("#imagem").attr("src", `${result.hdurl}`)
+        }
+    })
 
+})
